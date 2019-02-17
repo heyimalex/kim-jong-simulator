22f548bcfb5ef9a9dff6e227da3ccea672506947
Alex Guerra <alex@heyimalex.com>
2019-02-17 15:29:50 -0600

    Split rendering into stages for perf measurement

--- src/pipeline/index.ts
+++ src/pipeline/index.ts
@@ -19,6 +19,11 @@ export function loadDeps(): Promise<Dependencies> {
   ]).then(([textures, baseImg]) => ({ textures, baseImg }));
 }
 
+interface RenderArgs {
+  text: string;
+  target: HTMLCanvasElement;
+}
+
 export function createRenderer(deps: Dependencies) {
   const textCanvas = document.createElement("canvas");
   textCanvas.width = GRID_WIDTH;
@@ -34,57 +39,93 @@ export function createRenderer(deps: Dependencies) {
 
   const kjs = new KJS(deps.textures);
 
+  const stages = [
+    ({ text }: RenderArgs) => {
+      if (textCanvasCtx === null) {
+        throw new Error("textCanvas.getContext returned null");
+      }
+      // RENDER TEXT
+      const lines = text
+        .toUpperCase()
+        .split(/[\r\n]+/)
+        .map(l => l.trim())
+        .filter(l => l !== "");
+      textCanvasCtx.clearRect(0, 0, GRID_WIDTH, GRID_HEIGHT);
+      if (lines.length !== 0) {
+        fitText(textCanvasCtx, 7, 110 + 10, 180 - 14, 110 - 20, lines);
+        quantizeTextCanvas(
+          textCanvasCtx,
+          0,
+          0,
+          GRID_WIDTH,
+          GRID_HEIGHT,
+          [255, 255, 255],
+          255
+        );
+      }
+    },
+    () => {
+      if (preRenderCanvasCtx === null) {
+        throw new Error("preRenderCanvas.getContext returned null");
+      }
+      preRenderCanvasCtx.drawImage(deps.baseImg, 0, 0);
+      preRenderCanvasCtx.drawImage(textCanvas, 0, 0);
+    },
+    () => {
+      if (preRenderCanvasCtx === null) {
+        throw new Error("preRenderCanvas.getContext returned null");
+      }
+      quantizeImageDataToGrid(
+        preRenderCanvasCtx.getImageData(0, 0, GRID_WIDTH, GRID_HEIGHT),
+        preRenderGrid
+      );
+    },
+    () => {
+      kjs.updateScene(preRenderGrid);
+    },
+    ({ target }: RenderArgs) => {
+      kjs.renderScene();
+
+      const targetCtx = target.getContext("2d");
+      if (targetCtx === null) {
+        throw new Error("targetCanvas.getContext returned null");
+      }
+
+      // DRAW THE OUTPUT IMAGE
+      targetCtx.drawImage(kjs.renderer.domElement, 0, 0);
+    }
+  ];
+
   function render(text: string, target: HTMLCanvasElement) {
-    if (textCanvasCtx === null) {
-      throw new Error("textCanvas.getContext returned null");
-    }
-    if (preRenderCanvasCtx === null) {
-      throw new Error("preRenderCanvas.getContext returned null");
-    }
-
-    const targetCtx = target.getContext("2d");
-    if (targetCtx === null) {
-      throw new Error("targetCanvas.getContext returned null");
-    }
-
     console.log("render called with", text, target);
 
-    // RENDER TEXT
-    const lines = text
-      .toUpperCase()
-      .split(/[\r\n]+/)
-      .map(l => l.trim())
-      .filter(l => l !== "");
-    textCanvasCtx.clearRect(0, 0, GRID_WIDTH, GRID_HEIGHT);
-    if (lines.length !== 0) {
-      fitText(textCanvasCtx, 7, 110 + 10, 180 - 14, 110 - 20, lines);
-      quantizeTextCanvas(
-        textCanvasCtx,
-        0,
-        0,
-        GRID_WIDTH,
-        GRID_HEIGHT,
-        [255, 255, 255],
-        1
-      );
+    let cancelled = false;
+    let i = 0;
+    let timeoutId: number = 0;
+    const args: RenderArgs = {
+      text,
+      target
+    };
+    const TIMEOUT = 10;
+    function nextStage() {
+      if (cancelled) return;
+      const stage = stages[i];
+      if (stage === undefined) return;
+
+      const start = performance.now();
+      stage(args);
+      const end = performance.now();
+      console.log(`stage ${i + 1} completed in ${end - start}`);
+      i += 1;
+      if (i < stages.length) {
+        timeoutId = window.setTimeout(nextStage, TIMEOUT);
+      }
     }
-
-    // RENDER TO PRE-RENDER-CANVAS
-    preRenderCanvasCtx.drawImage(deps.baseImg, 0, 0);
-    preRenderCanvasCtx.drawImage(textCanvas, 0, 0);
-
-    // COPY TO GRID
-    quantizeImageDataToGrid(
-      preRenderCanvasCtx.getImageData(0, 0, GRID_WIDTH, GRID_HEIGHT),
-      preRenderGrid
-    );
-
-    // UPDATE KJS WITH NEW GRID
-    kjs.updateScene(preRenderGrid);
-    kjs.renderScene();
-
-    // DRAW THE OUTPUT IMAGE
-    targetCtx.drawImage(kjs.renderer.domElement, 0, 0);
+    timeoutId = window.setTimeout(nextStage, TIMEOUT);
+    return () => {
+      cancelled = true;
+      window.clearTimeout(timeoutId);
+    };
   }
   return render;
 }
